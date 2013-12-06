a87755accdff1233bb99894409778c5ba952202b
dreammaster82 <dreammaster82@gmail.com>
2013-12-06 12:44:00 +0400

    new core

--- www/include/config.php
+++ www/include/config.php
@@ -16,6 +16,7 @@ $CONFIG = array(
 	'title' => '',
 	'debug' => 1,
 	'debug_ip' => array('127.0.0.1'),
-	'no_apache' => 1
+	'no_apache' => 1,
+	'memcache_priority' => 1 //Приоритет классов 0 - Memcache, 1 - Memcached
 );
 ?>
---
+++ www/include/scroll.php
@@ -0,0 +1,50 @@
+<?php
+class Scroll{
+	public $limit = 12, $cnt = 20, $pagesInLine = 3, $page;
+
+	function __construct() {
+		$this->page = (int)$_REQUEST['page'];
+	}
+
+	function showPages($lnk = ''){
+		if(!$this->cnt){
+			return false;
+		}
+		/*$data = array();
+		$out['iteration'] = ceil($this->cnt / $this->limit);
+		if($out['iteration'] == 1){
+			return false;
+		}
+		$first = floor($this->page / $this->pagesInLine) *  $this->pagesInLine;
+		if($out['iteration'] <= $this->pagesInLine || $out['iteration'] <= ($first + $this->pagesInLine)){
+			$out['end'] = $out['iteration'];
+		} else {
+			$out['end'] = $first + $this->pagesInLine;
+		}
+		$out['next'] = $first;
+		if(($this->page + 1) > $this->pagesInLine) {
+			$out['cur_page'] = $out['next'] - 1;
+			$out['active'] = ' active';
+		} else {
+			$out['page'] = $out['next'];
+			$out['cur_page'] = 0;
+		}*/
+		if($lnk){
+			if(strpos($lnk, '?') === false){
+				$out['href'] = $lnk.'?page=';
+			} else {
+				$out['href'] = $lnk.'&page=';
+			}
+		} else {
+			$out['href'] = './?page=';
+		}
+		if(($this->cnt - $this->limit * ($this->page + 1)) > 0){
+			ob_start();
+			include CLIENT_PATH.'/data/show_newpages.html';
+			return ob_get_clean();
+		} else {
+			return '';
+		}
+	}
+}
+?>
