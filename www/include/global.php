f558f1606c8149b7d62e346168aa1002f153e5f0
Denis <test@test.ru>
2018-03-16 23:17:54 +0300

    меню и body класс

--- www/include/global.php
+++ www/include/global.php
@@ -4,24 +4,35 @@ if(!$it = $Core->getClass('Memcache')->get('getContentItems')){
 		$it = $C->getContentItems();
 	}
 }
+$out['nav'] = [];
 if($it){
-	$arr = [];
-	$arr1 = [];
-	foreach ($it as $k => $v){
-		$arr[$v[1]][] = [$v[2], $v[3]];
-		if($v[4]){
-			$arr1[$v[0]] = [$v[2], $v[3]];
-		}
-	}
-	unset($items);
-	if($arr1){
-		foreach ($arr1 as $k => $v){
-			if($arr[$k]){
-				$arr1[$k][2] = $arr[$k];
-			}
-		}
-		unset($arr);
-		$out['nav'] = $arr1;
-	}
+    $cur = (int)$_REQUEST['req'];
+    $arr = [];
+    $arr1 = [];
+    foreach ($it as $k => $v){
+        if($v[3]){
+            if(!$v[1]) $arr1[$v[0]] = [$Core->getClass('Util')->getAlias($v[0], 'content'), $v[2], $v[0], $cur == $v[0]];
+            else {
+                $arr[$v[1]][$v[0]] = [$Core->getClass('Util')->getAlias($v[1], 'content') . '/' . $Core->getClass('Util')->getAlias($v[0], 'content'), $v[2], $v[0], $cur == $v[0]];
+                if($cur == $v[0]) $cur = (int)$v[1];
+            }
+        }
+    }
+    unset($it);
+    if($arr1){
+        foreach ($arr1 as $k => $v){
+            if($arr[$k]){
+                if($cur == $k) $arr1[$k][3] = true;
+                $arr1[$k][4] = $arr[$k];
+            }
+        }
+        unset($arr);
+        $out['nav'] = $arr1;
+    }
 }
+
+$bodyClass = [];
+if((int)$_REQUEST['page']) array_push($bodyClass, 'paged');
+array_push($bodyClass, $out['active'] == 'index' ? 'main-page' : $_REQUEST['module']);
+$out['body-class'] = join(' ', $bodyClass);
 ?>
